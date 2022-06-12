a5417022bd74b7765a60f6cac92f1b39eaca5e0c
Dhananjay <dhananjay.latkar@gmail.com>
2022-06-12 13:39:50 +0530

    Time: 4 ms (41.50%), Space: 6 MB (51.26%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        
+        for (int i = 0; i < 32; i++)
+        {
+            
+            if(n & (1 << i))
+            {
+                res = res | (1 << (31-i));
+            }
+        }
+        
+        return res;
+    }
+};

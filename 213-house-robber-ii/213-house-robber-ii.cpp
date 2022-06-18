665c268e19beeb1b596659db13f14c805613b7b2
Dhananjay <dhananjay.latkar@gmail.com>
2022-06-18 22:18:37 +0530

    Time: 3 ms (56.61%), Space: 7.7 MB (97.29%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int res = -1;
+    int dp[1001][2];
+    int helper(vector<int>& nums, int idx, bool visitedFirst)
+    {
+        int n = nums.size();
+        
+        if (idx >= n)
+        {
+            return 0;
+        }
+
+        if (dp[idx][visitedFirst] != -1) return dp[idx][visitedFirst];
+
+        int p1 = helper(nums, idx+1, visitedFirst);
+        int p2 = 0;
+        if (idx == 0)
+        {
+            p2 += nums[idx] + helper(nums, idx+2, true);
+        }
+        else if (idx == n-1)
+        {
+            if (visitedFirst == false)
+            {
+                p2 += nums[idx] + helper(nums, idx+2, visitedFirst);
+            }
+        }
+        else
+        {
+            p2 += nums[idx] + helper(nums, idx+2, visitedFirst);
+        }
+        dp[idx][visitedFirst] = max(p1, p2);
+        return dp[idx][visitedFirst];
+    }
+    int rob(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        return helper(nums, 0, false);
+    }
+};

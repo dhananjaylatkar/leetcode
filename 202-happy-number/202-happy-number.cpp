76911513cb0fdc9d6e0d1df69a3a6af2801648b0
Dhananjay <dhananjay.latkar@gmail.com>
2022-06-24 23:36:49 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (25.45%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int orig;
+    unordered_set<int> s;
+    bool helper(int n)
+    {
+        if (n == 1) return true;
+        if (s.count(n)) return false;
+        else
+        {
+            s.insert(n);
+        }
+        
+        int tmp = 0;
+        while (n)
+        {
+            tmp += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        
+        return helper(tmp);
+    }
+    bool isHappy(int n) {
+        return helper(n);
+    }
+};

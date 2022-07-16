adf8939f4fa1a339c0cb21415aaf24f0c6f9f31c
Dhananjay <dhananjay.latkar@gmail.com>
2022-07-16 12:41:12 +0530

    Time: 3 ms (58.27%), Space: 7.8 MB (61.89%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
-    int dp[1001][2];
-    int helper(vector<int>& nums, int idx, bool visitedFirst)
+    int dp[101][2];
+    int helper(vector<int>& nums, int idx, bool robbed_first)
     {
         int n = nums.size();
         
-        if (idx >= n)
-        {
-            return 0;
-        }
-
-        if (dp[idx][visitedFirst] != -1) return dp[idx][visitedFirst];
-
-        int p1 = helper(nums, idx+1, visitedFirst);
-        int p2 = 0;
+        if (idx >= n) return 0;
+        
+        if (dp[idx][robbed_first] != -1) return dp[idx][robbed_first];
+        
+        if (idx == n-1 && robbed_first) return 0;
+        int p1 = 0, p2 = 0;
         if (idx == 0)
         {
-            p2 += nums[idx] + helper(nums, idx+2, true);
+            p1 = nums[idx] + helper(nums, idx+2, true);
+            p2 = helper(nums, idx+1, false);
         }
-        else if (idx == n-1)
+        else 
         {
-            if (visitedFirst == false)
-            {
-                p2 += nums[idx] + helper(nums, idx+2, visitedFirst);
-            }
+            p1 = nums[idx] + helper(nums, idx+2, robbed_first);
+            p2 = helper(nums, idx+1, robbed_first);
         }
-        else
-        {
-            p2 += nums[idx] + helper(nums, idx+2, visitedFirst);
-        }
-        dp[idx][visitedFirst] = max(p1, p2);
-        return dp[idx][visitedFirst];
+        
+        return dp[idx][robbed_first] = max(p1, p2);
     }
     int rob(vector<int>& nums) {
         memset(dp, -1, sizeof(dp));

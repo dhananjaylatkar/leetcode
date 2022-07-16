5276c5cd81bc260a0abc02ba2eee8f42eced2594
Dhananjay <dhananjay.latkar@gmail.com>
2022-07-16 12:27:20 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (75.68%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
-    
-//     int helper(vector<int>& nums, int curr)
-//     {
-//         int n = nums.size();
-//         if (curr >= n)
-//         {
-//             return 0;
-//         }
-        
-//         int p1 = nums[curr] + helper(nums, curr+2);
-//         int p2 = helper(nums, curr+1);
-        
-//         return max(p1, p2);
-            
-//     }
     int dp[101];
-    int helper_dp(vector<int>& nums, int curr)
+    int helper(vector<int>& nums, int idx)
     {
         int n = nums.size();
-        if (curr >= n)
-        {
-            return 0;
-        }
-        if (dp[curr] != -1) return dp[curr];
-        
-        int p1 = nums[curr] + helper_dp(nums, curr+2);
-        int p2 = helper_dp(nums, curr+1);
-        
-        dp[curr] = max(p1, p2);
-        return dp[curr];
-            
+        if (idx >= n) return 0;
+        if (dp[idx] != -1) return dp[idx];
+        int p1 = nums[idx] + helper(nums, idx+2);
+        int p2 = helper(nums, idx+1);
+        return dp[idx] = max(p1, p2);
     }
     int rob(vector<int>& nums) {
-        // return helper(nums, 0);
         memset(dp, -1, sizeof(dp));
-        return helper_dp(nums, 0);
+        
+        return helper(nums, 0);
     }
 };

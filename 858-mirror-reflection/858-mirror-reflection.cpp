5344da2dd607770ed15a6ed82f2b8c6113583bbb
Dhananjay <dhananjay.latkar@gmail.com>
2022-08-04 20:48:38 +0530

    Time: 3 ms (36.36%), Space: 5.9 MB (62.81%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mirrorReflection(int p, int q) {
+        while (p%2 == 0 && q%2 == 0)
+        {
+            p = p/2;
+            q = q/2;
+        }
+        
+        if (p%2 == 0 && q%2 != 0)
+        {
+            return 2;
+        }
+        
+        if (p%2 != 0 && q%2 == 0)
+        {
+            return 0;
+        }
+        
+        if (p%2 != 0 && q%2 != 0)
+        {
+            return 1;
+        }
+        return -1;
+    }
+};

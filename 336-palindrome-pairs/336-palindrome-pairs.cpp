58655521d67a671654c6d8efa4d5e28cfcacabd2
Dhananjay <dhananjay.latkar@gmail.com>
2022-09-17 13:32:46 +0530

    Time: 1856 ms (44.40%), Space: 594.4 MB (18.91%) - LeetHub

---
+++ 336-palindrome-pairs/336-palindrome-pairs.cpp
@@ -0,0 +1,75 @@
+/*
+Approach:
+
+    Idea is to use Trie to store all the words in reverse order
+    For each word, we can check if the current node is a palindrome, if so, we can add all the words in the current node to the answer.
+    We can also check if the current node is a prefix of the current word, if so, we can check if the remaining part of the word is a palindrome, if so, we can add the current word to the answer.
+    We can also check if the current node is a suffix of the current word, if so, we can check if the remaining part of the word is a palindrome, if so, we can add the current word to the answer.
+    We can also check if the current node is a word, if so, we can add the current word to the answer.
+    So that we can find all the palindromes in O(k) time
+*/
+struct TrieNode
+{
+    TrieNode *child[26]={};
+    int curIndex=-1;
+    vector<int> wordIndex;
+};
+class Solution
+{
+    bool isPalindrome(string &s, int i, int j)
+    {
+        while (i < j)
+        {
+            if (s[i++] != s[j--])
+                return false;
+        }
+        return true;
+    }
+    TrieNode *root;
+    void insert(string &s, int index)
+    {
+        TrieNode *cur = root;
+        for (int i = s.size() - 1; i >= 0; i--)
+        {
+            int c = s[i] - 'a';
+            if (cur->child[c] == nullptr)
+                cur->child[c] = new TrieNode();
+            if (isPalindrome(s, 0, i))
+                cur->wordIndex.push_back(index);
+            cur = cur->child[c];
+        }
+        cur->wordIndex.push_back(index);
+        cur->curIndex = index;
+    }
+
+public:
+    vector<vector<int>> palindromePairs(vector<string> &words)
+    {
+        root = new TrieNode();
+        for (int i = 0; i < words.size(); i++)
+            insert(words[i], i);
+        vector<vector<int>> ans;
+        for (int i = 0; i < words.size(); i++)
+        {
+            TrieNode *cur = root;
+            string &s = words[i];
+            for (int j = 0; j < s.size(); j++)
+            {
+                if (cur->curIndex != -1 && cur->curIndex != i && isPalindrome(s, j, s.size() - 1))
+                    ans.push_back({i, cur->curIndex});
+                cur = cur->child[s[j] - 'a'];
+                if (cur == nullptr)
+                    break;
+            }
+            if (cur == nullptr)
+                continue;
+            for (int j : cur->wordIndex)
+            {
+                if (i == j)
+                    continue;
+                ans.push_back({i, j});
+            }
+        }
+        return ans;
+    }
+};

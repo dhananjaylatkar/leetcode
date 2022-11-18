19740c50b5181dc85824560ac6756e2644fb7eb8
Dhananjay <dhananjay.latkar@gmail.com>
2022-11-18 18:08:15 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (19.97%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        while (n && n%5 == 0)
+        {
+            n /= 5;
+        }
+        
+        while (n && n%3 == 0)
+        {
+            n /= 3;
+        }
+        
+        while (n && n%2 == 0)
+        {
+            n /= 2;
+        }
+        
+        return n==1;
+    }
+};
